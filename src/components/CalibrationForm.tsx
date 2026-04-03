f378da3df0568a906d034558c3e88c6aa46ed8c7
RocaBOT <julia@datarac.es>
2026-04-03 23:26:07 +0200

    better seedlist centering solution

--- src/components/CalibrationForm.tsx
+++ src/components/CalibrationForm.tsx
@@ -537,7 +537,14 @@ export default function CalibrationForm({
                     ]
                 }</TextField>
             <Autocomplete
-                options={seedList}
+                options={seedList.slice(
+                        Math.max(targetSeedIndex - 50, 0), 
+                        Math.min(targetSeedIndex + 50, seedList.length)
+                    ).concat(
+                        seedList.slice(0, Math.max(targetSeedIndex - 51, 0))
+                    ).concat(
+                        seedList.slice(Math.min(targetSeedIndex + 51, seedList.length), seedList.length)
+                    )}
                 value={targetSeed}
                 onChange={(_event, newValue) => {
                     setCalibrationURLState({
@@ -551,12 +558,7 @@ export default function CalibrationForm({
                         gameConsole
                     )}ms)`;
                 }}
-                filterOptions={(option, _) => targetSeedFilterOptions(option.slice(
-                    Math.max(targetSeedIndex - 50, 0),
-                    Math.min(
-                        targetSeedIndex + 50,
-                        seedList.length
-                    )), _)}
+                filterOptions={targetSeedFilterOptions}
                 renderInput={(params) => (
                     <TextField
                         {...params}
